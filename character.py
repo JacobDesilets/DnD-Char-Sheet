24f3c5bb7f83ef48381be979f0ecaea4b360a1d0
Jacob Desilets <jacobdesilets@gmail.com>
2020-05-19 17:20:07 -0400

    Decided on Python, started Character class

---
+++ character.py
@@ -0,0 +1,22 @@
+class Character:
+    def __init__(self):
+        self.name = "DEFAULT"
+        self.char_class = "DEFAULT"
+        self.race = "DEFAULT"
+        self.level = 999
+        self.xp = 999
+
+        self.stat_str = 999
+        self.stat_dex = 999
+        self.stat_con = 999
+        self.stat_int = 999
+        self.stat_wis = 999
+        self.stat_cha = 999
+
+        self.mod_str = 999
+        self.mod_dex = 999
+        self.mod_con = 999
+        self.mod_int = 999
+        self.mod_wis = 999
+        self.mod_cha = 999
+
